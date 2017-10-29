7843ee53c182d9736d51f12ed82866b148cf7753
Sid Premkumar <sidprem@bu.edu>
2017-10-29 11:46:31 -0400

    update2

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ server.listen(3978, () => {
 })
 server.post('/api/messages', connector.listen())
 
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 
 //series of dialogs
 bot.dialog('/', [
